a3dbd3e44313a1f17f12dc6d3c34d17d3357d6d6
Bhavesh sonagra <sonagrabhavesh@gmail.com>
2022-11-24 12:34:11 +0530

    Update Code

--- wappclone/settings.py
+++ wappclone/settings.py
@@ -103,17 +103,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {  
-    'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'wapp',  
-        'USER':'root',  
-        'PASSWORD':'root',  
-        'HOST':'localhost',  
-        'PORT':'3306'  
-    }  
-}  
+# DATABASES = {  
+#     'default': {  
+#         'ENGINE': 'django.db.backends.mysql',  
+#         'NAME': 'wapp',  
+#         'USER':'root',  
+#         'PASSWORD':'root',  
+#         'HOST':'localhost',  
+#         'PORT':'3306'  
+#     }  
+# }  
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
