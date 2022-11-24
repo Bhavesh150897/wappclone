c54cbb14f38468b48ab69b68ad8cf04821bfc67a
Bhavesh sonagra <sonagrabhavesh@gmail.com>
2022-11-24 12:27:31 +0530

    Update Code

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from chat import consumers
 from django.urls import re_path
 
 websocket_urlpatterns = [
-    re_path(r'^ws$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^ws$', consumers.ChatConsumer),
 ]
