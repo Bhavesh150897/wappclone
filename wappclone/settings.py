685495412bad4a8c110a8146ca888f1bd41b7ce2
Bhavesh sonagra <sonagrabhavesh@gmail.com>
2022-12-01 12:41:52 +0530

    Update Code

--- wappclone/settings.py
+++ wappclone/settings.py
@@ -94,12 +94,6 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels.layers.InMemoryChannelLayer"
     }
 }
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
-}
 # CHANNEL_LAYERS = {
 #     'default': {
 #         'BACKEND': 'channels_redis.core.RedisChannelLayer',
